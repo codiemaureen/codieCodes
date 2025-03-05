6bc9372fcdd72a491b33fe6c97688b278ffa7c9b
Codiemaureen <codiestover.cs@gmail.com>
2025-03-05 14:26:00 -0500

    Moving header code to header component

---
+++ src/app/component/Header.jsx
@@ -0,0 +1,19 @@
+import styles from './Header.module.css';
+
+const Header = () => {
+  return (
+    <div className={styles.navContainer}>
+      <h1 className={styles.headerTitle}>Codie Groth</h1>
+      <div className={styles.navItem}>ABOUTME.md</div>
+      <div className={styles.navItem}>Github.com</div>
+      <div className={styles.navItem}>LinkedIn.com</div>
+      <div className={styles.navItem}>Projects</div>
+      <div className={styles.navItem}>Contact Me</div>
+      <header className={styles.header}>
+        <img src='/pexels-pixabay-270632.jpg' alt='' className={styles.headerImage} />
+      </header>
+    </div>
+  )
+}
+
+export default Header
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,11 @@
+import Header from "./component/Header";
 import AboutMe from "./component/AboutMe";
 import Projects from "./component/Projects";
 
 export default function Home() {
   return (
     <>
-    <div className="nav-container">
-      <h1>Codie Groth</h1>
-      <div className="nav-item">ABOUTME.md</div>
-      <div className="nav-item">Github.com</div>
-      <div className="nav-item">LinkedIn.com</div>
-      <div className="nav-item">Projects</div>
-      <div className="nav-item">Contact Me</div>
-      <header className="header">
-        <img src='/pexels-pixabay-270632.jpg' alt="" className="header-image" />
-      </header>
-    </div>
+    <Header />
     <AboutMe />
     <Projects />
     </>
