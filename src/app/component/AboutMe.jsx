979ed0f42b280b16db951fefe7be05695bfd06ac
Codiemaureen <codiestover.cs@gmail.com>
2025-03-05 12:47:52 -0500

    Adding flexibility to about me

--- src/app/component/AboutMe.jsx
+++ src/app/component/AboutMe.jsx
@@ -1,7 +1,10 @@
+import styles from './AboutMe.module.css';
+
 const AboutMe = () => {
   return (
-    <div>
-      <h2 className="aboutHeader">About Me</h2>
+    <div className={styles.aboutMe}>
+      <h2 className={styles.aboutHeader}>{`<About Me />`}</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi excepturi accusantium doloremque ipsa atque iure iusto ullam culpa aliquid nobis animi porro cumque optio omnis dolorem quia, similique labore provident. Accusamus non perferendis voluptatem possimus ex accusantium aliquid consequuntur ullam?</p>
     </div>
   )
 }
