243261fbdc8301c328d0ddbf73cfd823f8f98a17
Codiemaureen <codiestover.cs@gmail.com>
2025-03-05 11:56:28 -0500

    Adding animation to header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,9 @@
-import headerImage from '../../public/pexels-pixabay-270632.jpg'
+
 
 export default function Home() {
   return (
+    <>
+    
   <div className="nav-container">
    <h1>Codie Groth</h1>
    <div className="nav-item">ABOUTME.md</div>
@@ -9,9 +11,10 @@ export default function Home() {
    <div className="nav-item">LinkedIn.com</div>
    <div className="nav-item">Projects</div>
    <div className="nav-item">Contact Me</div>
-   <header className="header">
-     <img src={headerImage} alt="" className="header-image" />
-   </header>
   </div>
+   <header className="header">
+    <img src='/pexels-pixabay-270632.jpg' alt="" className="header-image" />
+   </header>
+   </>
   );
 }
