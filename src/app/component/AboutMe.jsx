1335063d8d27bbce40990fd015811f42ebf1c29f
Codiemaureen <codiestover.cs@gmail.com>
2025-03-05 12:06:56 -0500

    Adding about me component

---
+++ src/app/component/AboutMe.jsx
@@ -0,0 +1,9 @@
+const AboutMe = () => {
+  return (
+    <div>
+      <h2 className="aboutHeader">About Me</h2>
+    </div>
+  )
+}
+
+export default AboutMe
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,21 @@
-
+import AboutMe from "./component/AboutMe";
 
 export default function Home() {
   return (
     <>
-    
-  <div className="nav-container">
-   <h1>Codie Groth</h1>
-   <div className="nav-item">ABOUTME.md</div>
-   <div className="nav-item">Github.com</div>
-   <div className="nav-item">LinkedIn.com</div>
-   <div className="nav-item">Projects</div>
-   <div className="nav-item">Contact Me</div>
-  </div>
-   <header className="header">
-    <img src='/pexels-pixabay-270632.jpg' alt="" className="header-image" />
-   </header>
-   </>
+    <div className="nav-container">
+      <h1>Codie Groth</h1>
+      <div className="nav-item">ABOUTME.md</div>
+      <div className="nav-item">Github.com</div>
+      <div className="nav-item">LinkedIn.com</div>
+      <div className="nav-item">Projects</div>
+      <div className="nav-item">Contact Me</div>
+      <header className="header">
+        <img src='/pexels-pixabay-270632.jpg' alt="" className="header-image" />
+      </header>
+    </div>
+    <AboutMe />
+    </>
+
   );
 }
