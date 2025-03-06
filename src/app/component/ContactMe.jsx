6f8893ab44b3280396845b8070c82221a445b402
Codiemaureen <codiestover.cs@gmail.com>
2025-03-06 17:18:35 -0500

    Adding form structure

--- src/app/component/ContactMe.jsx
+++ src/app/component/ContactMe.jsx
@@ -3,7 +3,29 @@ import styles from './ContactMe.module.css';
 const ContactMe = () => {
   return (
     <div className={styles.contactContainer}>
-     <h2 className={styles.contactHeader}>{'<contact form />'}</h2>
+     <h2 className={styles.contactHeader}>{'<contact me />'}</h2>
+
+
+     <form action="" method="get" className={styles.form}>
+       <h2 className={styles.formHeading}>Send me an Email!</h2>
+       <div className={styles.formExample}>
+         <label htmlFor="name">Enter your name: </label>
+         <input type="text" name="name" id="name" required />
+       </div>
+       <div className={styles.formExample}>
+         <label htmlFor="email">Enter your email: </label>
+         <input type="email" name="email" id="email" required />
+       </div>
+       <div className={styles.formExample}>
+         <label htmlFor="email"> </label>
+       </div>
+       <div className={styles.formExample}>
+         <textarea name="emailBody" id="emailBody" rows="15" cols="50" value='Email body...'/>
+       </div>
+       <div className={styles.formExample}>
+         <input type="submit" value="Contact Me" className={styles.formInput}/>
+       </div>
+     </form>
     </div>
   )
 }
