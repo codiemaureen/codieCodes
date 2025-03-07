d2f0a153d560181e0387dc14e3afc26bab4a158a
Codiemaureen <codiestover.cs@gmail.com>
2025-03-07 12:20:18 -0500

    Adding my details

--- src/app/component/MyDetails.jsx
+++ src/app/component/MyDetails.jsx
@@ -4,13 +4,13 @@ const MyDetails = () => {
   return (
     <div className={styles.detailsContainer}>
       <ol className={styles.detailsList}>
-        <li className={styles.detailItems}></li>
-        <li className={styles.detailItems}></li>
-        <li className={styles.detailItems}></li>
-        <li className={styles.detailItems}></li>
-        <li className={styles.detailItems}></li>
-        <li className={styles.detailItems}></li>
-        <li className={styles.detailItems}></li>
+        <li className={styles.detailItems}>Full Stack Software Engineer</li>
+        <li className={styles.detailItems}>Metro Detroit</li>
+        <li className={styles.detailItems}>CodieGroth@Gmail.com</li>
+        <li className={styles.detailItems}>Github</li>
+        <li className={styles.detailItems}>LinkedIn</li>
+        <li className={styles.detailItems}>Schedue a Coffee Chat</li>
+        <li className={styles.detailItems}>Vist My Github City</li>
       </ol>
     </div>
   )
