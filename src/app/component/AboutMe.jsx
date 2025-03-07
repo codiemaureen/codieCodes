6a7c85b93e1182f7f15c0d1ebe99f0c0bcdee0dd
Codiemaureen <codiestover.cs@gmail.com>
2025-03-07 11:48:01 -0500

    Removing class to support global styling

--- src/app/component/AboutMe.jsx
+++ src/app/component/AboutMe.jsx
@@ -3,7 +3,7 @@ import styles from './AboutMe.module.css';
 const AboutMe = () => {
   return (
     <div className={styles.aboutMe}>
-      <h2 className={styles.aboutHeader}>{`<About Me />`}</h2>
+      <h2>{`<About Me />`}</h2>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi excepturi accusantium doloremque ipsa atque iure iusto ullam culpa aliquid nobis animi porro cumque optio omnis dolorem quia, similique labore provident. Accusamus non perferendis voluptatem possimus ex accusantium aliquid consequuntur ullam?</p>
     </div>
   )
--- src/app/component/Projects.jsx
+++ src/app/component/Projects.jsx
@@ -3,7 +3,7 @@ import styles from "./Projects.module.css";
 const Projects = () => {
   return (
     <div className={styles.projects}>
-      <h2 className={styles.projectsHeader}>{`<Projects />`}</h2>
+      <h2>{`<Projects />`}</h2>
       <div className={styles.projectsContainer}>
        <item className={styles.projectsItems}>item 1</item>
        <item className={styles.projectsItems}>item 2</item>
