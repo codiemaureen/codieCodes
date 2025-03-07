4c0faff5940019fa5af887e2b5d0ba6dad81e91f
Codiemaureen <codiestover.cs@gmail.com>
2025-03-07 12:11:16 -0500

    adding wrapper to component

--- src/app/component/AboutMe.jsx
+++ src/app/component/AboutMe.jsx
@@ -1,10 +1,16 @@
 import styles from './AboutMe.module.css';
+import MyDetails from './MyDetails';
+import MySkills from './MySkills';
 
 const AboutMe = () => {
   return (
     <div className={styles.aboutMe}>
-      <h2>{`<About Me />`}</h2>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi excepturi accusantium doloremque ipsa atque iure iusto ullam culpa aliquid nobis animi porro cumque optio omnis dolorem quia, similique labore provident. Accusamus non perferendis voluptatem possimus ex accusantium aliquid consequuntur ullam?</p>
+      <div className={styles.Blurb}>
+        <h2>{`<About Me />`}</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi excepturi accusantium doloremque ipsa atque iure iusto ullam culpa aliquid nobis animi porro cumque optio omnis dolorem quia, similique labore provident. Accusamus non perferendis voluptatem possimus ex accusantium aliquid consequuntur ullam?</p>
+      </div>
+      <MyDetails />
+      <MySkills />
     </div>
   )
 }
