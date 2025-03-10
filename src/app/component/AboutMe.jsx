8aecd1f2c6bc908c390aa239dfc682d8826571e9
Codiemaureen <codiestover.cs@gmail.com>
2025-03-10 11:17:55 -0400

    Adding skills and details column styles

--- src/app/component/AboutMe.jsx
+++ src/app/component/AboutMe.jsx
@@ -6,8 +6,8 @@ const AboutMe = () => {
   return (
     <div className={styles.aboutMe}>
       <MyDetails />
-      <div className={styles.Blurb}>
-        <h2>{`<About Me />`}</h2>
+      <div className={styles.blurb}>
+        <h2 className={styles.aboutHeader}>{`<About Me />`}</h2>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi excepturi accusantium doloremque ipsa atque iure iusto ullam culpa aliquid nobis animi porro cumque optio omnis dolorem quia, similique labore provident. Accusamus non perferendis voluptatem possimus ex accusantium aliquid consequuntur ullam?</p>
       </div>
       <MySkills />
