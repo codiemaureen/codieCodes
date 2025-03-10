8274a405740fc621494a6cfbe548001b7719d47c
Codiemaureen <codiestover.cs@gmail.com>
2025-03-10 10:17:18 -0400

    Adding hoverHandle fn

--- src/app/component/Project.jsx
+++ src/app/component/Project.jsx
@@ -1,9 +1,32 @@
+"use client";
+import { useState } from 'react';
 import styles from './Project.module.css';
+import Link from 'next/link';
+
 
 const Project = () => {
+ const [isHovering, setIsHovering] = useState(false);
+
+ const handleMouseEnter = () => {
+  setIsHovering(true);
+ }
+
+ const handleMouseLeave = () => {
+  setIsHovering(false);
+ }
   return (
-   <div className={styles.ProjectContainer}>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum, minus!</p>
+   <div className={styles.ProjectContainer} 
+   onMouseEnter={handleMouseEnter}
+   onMouseLeave={handleMouseLeave}>
+    <h3 >Project Name</h3>
+    {/* <img src="" alt="" /> */}
+    {isHovering ? 'Back Image' : 'Front Image'}
+    
+    <section className={styles.projectLinks}>
+     <Link href={'/'}>Github</Link>
+     <Link href={'/'}>WebSite</Link>
+     <Link href={'/'}>Mobile Preview</Link>
+    </section>
    </div>
   )
 }
