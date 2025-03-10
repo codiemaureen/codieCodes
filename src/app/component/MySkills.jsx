1020c9d1926889b91ba5fd9b9515e34291da9da6
Codiemaureen <codiestover.cs@gmail.com>
2025-03-10 14:03:34 -0400

    Adding final style touches

--- src/app/component/MySkills.jsx
+++ src/app/component/MySkills.jsx
@@ -4,6 +4,7 @@ const MySkills = () => {
   return (
     <div className={styles.skillsContainer}>
       <ol className={styles.skillList}>
+        <h4 className={styles.listTitle}>My Languages</h4>
         <li className={styles.skillItems}>NEXT.JS</li>
         <li className={styles.skillItems}>REACT</li>
         <li className={styles.skillItems}>ANGULAR</li>
