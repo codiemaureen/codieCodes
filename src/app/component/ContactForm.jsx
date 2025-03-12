3ebdec85e6b1bb4b1e491478c6278745a344207f
Codiemaureen <codiestover.cs@gmail.com>
2025-03-12 12:30:43 -0400

    Fixing mobile view on contact form

--- src/app/component/ContactForm.jsx
+++ src/app/component/ContactForm.jsx
@@ -4,23 +4,29 @@ const ContactForm = () => {
   return (
     <div className={styles.formContainer}>
      <form action="" method="get" className={styles.form}>
-       <h2 className={styles.formHeading}>Send me an Email!</h2>
-       <div className={styles.formExample}>
-         <label htmlFor="name">Enter your name: </label>
-         <input type="text" name="name" id="name" required />
+       <h2 className={styles.formHeading}>Let's Get in Touch!</h2>
+       <div className={styles.emailInput}>
+         <label className={styles.label}  htmlFor="email">Enter Your Email: </label>
+         <input type="email" name="email" id="email" placeholder="example@gmail.com" className={styles.formInput}
+         required />
        </div>
-       <div className={styles.formExample}>
-         <label htmlFor="email">Enter your email: </label>
-         <input type="email" name="email" id="email" required />
+       <div className={styles.emailInput}>
+         <label className={styles.label} htmlFor="email">Enter Your Email Subject: </label>
+         <input 
+          type="email" 
+          name="email" 
+          id="email" 
+          className={styles.formInput}
+          placeholder="Enter your subject here..." required />
        </div>
-       <div className={styles.formExample}>
-         <label htmlFor="email"> </label>
+       <div >
+       <div className={styles.textbody}>
+         <label className={styles.label} htmlFor="message">Enter Your Message:</label>
+         <textarea className={styles.textarea}name="emailBody" id="emailBody" placeholder='Enter your message here...'/>
        </div>
-       <div className={styles.formExample}>
-         <textarea className={styles.textarea}name="emailBody" id="emailBody" rows="15" cols="50" value='Email body...'/>
        </div>
-       <div className={styles.formExample}>
-         <input type="submit" value="Contact Me" className={styles.formInput}/>
+       <div >
+         <input type="submit" value="Send" className={`${styles.formInput} ${styles.button}`}/>
        </div>
      </form>
     </div>
