1da621058a171dbd0e38ba0ca050a00962209055
Codiemaureen <codiestover.cs@gmail.com>
2025-03-13 19:11:18 -0400

    adding links for my details

--- src/app/component/MyDetails.jsx
+++ src/app/component/MyDetails.jsx
@@ -6,11 +6,27 @@ const MyDetails = () => {
       <ol className={styles.detailsList}>
         <li className={styles.detailItems}>Full Stack Software Engineer</li>
         <li className={styles.detailItems}>Metro Detroit</li>
-        <li className={styles.detailItems}>CodieGroth@Gmail.com</li>
-        <li className={styles.detailItems}>Github</li>
-        <li className={styles.detailItems}>LinkedIn</li>
+        <li className={styles.detailItems}>    
+          <a href="mailto:codiegroth?subject=Hello%20there&body=I%20wanted%20to%20reach%20out">
+            CodieGroth@Gmail.com
+          </a>
+        </li>
+        <li className={styles.detailItems}>
+          <a href="https://github.com/codiemaureen" target='_blank'>
+            Github
+          </a>
+        </li>
+        <li className={styles.detailItems}>
+          <a href="https://github.com/codiemaureen" target='_blank'>
+            LinkedIn
+          </a>
+        </li>         
         <li className={styles.detailItems}>Schedule a Coffee Chat</li>
-        <li className={styles.detailItems}>Vist My Github City</li>
+        <li className={styles.detailItems}>
+          <a href="https://honzaap.github.io/GithubCity/?name=codiemaureen&year=2025" target='_blank'>
+            Visit My Github City
+          </a>
+        </li>
       </ol>
     </div>
   )
