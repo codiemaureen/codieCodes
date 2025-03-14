291c0d9c1cb5addb7f2f9d1d6d831bb1515fb714
Codiemaureen <codiestover.cs@gmail.com>
2025-03-14 09:50:57 -0400

    removing background and text colors, cleaning up code

--- src/app/component/AboutMe.jsx
+++ src/app/component/AboutMe.jsx
@@ -7,7 +7,7 @@ const AboutMe = () => {
     <div className={styles.aboutMe}>
       <MyDetails />
       <div className={styles.blurb}>
-        <h2 className={styles.aboutHeader}>{`<About Me />`}</h2>
+        <h2 className={styles.aboutHeader}>{`About Me`}</h2>
         <p className={styles.description}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi excepturi accusantium doloremque ipsa atque iure iusto ullam culpa aliquid nobis animi porro cumque optio omnis dolorem quia, similique labore provident. Accusamus non perferendis voluptatem possimus ex accusantium aliquid consequuntur ullam?</p>
       </div>
       <MySkills />
