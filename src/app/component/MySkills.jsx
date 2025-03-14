0a1bf54272be17a91921b21c896875216b196fde
Codiemaureen <codiestover.cs@gmail.com>
2025-03-14 11:30:58 -0400

    Cleaning up skills component

--- src/app/component/MySkills.jsx
+++ src/app/component/MySkills.jsx
@@ -5,12 +5,24 @@ const MySkills = () => {
     <div className={styles.skillsContainer}>
       <ol className={styles.skillList}>
         <h4 className={styles.listTitle}>My Languages</h4>
-        <li className={styles.skillItems}>NEXT.JS</li>
-        <li className={styles.skillItems}>REACT</li>
-        <li className={styles.skillItems}>ANGULAR</li>
-        <li className={styles.skillItems}>NODE</li>
-        <li className={styles.skillItems}>TYPESCRIPT</li>
-        <li className={styles.skillItems}>JAVASCRIPT</li>
+        <li className={styles.skillItems}>
+          NEXT.JS
+        </li>
+        <li className={styles.skillItems}>
+          REACT
+        </li>
+        <li className={styles.skillItems}>
+          ANGULAR
+        </li>
+        <li className={styles.skillItems}>
+          NODE
+        </li>
+        <li className={styles.skillItems}>
+          TYPESCRIPT
+        </li>
+        <li className={styles.skillItems}>
+          JAVASCRIPT
+        </li>
       </ol>
     </div>
   )
