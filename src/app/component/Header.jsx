2759497ab4c280dcdd8e1d3e46b221cec8a98a81
Codiemaureen <codiestover.cs@gmail.com>
2025-03-14 09:47:08 -0400

    Styling header and cleaning up code ✨

--- src/app/component/Header.jsx
+++ src/app/component/Header.jsx
@@ -1,23 +1,41 @@
 import styles from './Header.module.css';
 import Link from 'next/link';
+import { Fleur_De_Leah, DM_Serif_Text } from 'next/font/google';
+
+const fleurDeLeah = Fleur_De_Leah({
+  weight: '400',
+  subsets: ['latin'],
+});
+const dmSerifText = DM_Serif_Text({
+  weight: '400',
+  subsets: ['latin'],
+});
 
 const Header = () => {
   return (
     <>
-    
-    <div id='navBar' className={styles.navContainer}>
-      <Link className={styles.headerTitle} href="#navBar"><h1 className={styles.headerTitle}>Codie Groth</h1></Link>
-      <ol  className={styles.navList}>
-        <Link className={styles.navLink} href="https://github.com/codiemaureen" target='_blank'><li className={styles.navItem}>Github</li></Link>
-        <Link className={styles.navLink} href="https://www.linkedin.com/in/codiemaureen/" target='_blank'><li className={styles.navItem}>LinkedIn</li></Link>
-        <Link className={styles.navLink} href="#projects"><li className={styles.navItem}>Projects</li></Link>
-        <Link className={styles.navLink} href="#contactMe"><li className={styles.navItem}>Contact Me</li></Link>
-      </ol> 
-    </div>
+      <div id='navBar' className={styles.navContainer}>
+        <Link className={styles.headerTitle} href="#navBar"><
+          h1 className={`${styles.headerTitle} ${fleurDeLeah.className}`}>Codie Groth</h1>
+        </Link>
+        <ol className={`${styles.navList} ${dmSerifText.className}`}>
+          <Link className={styles.navLink} href="https://github.com/codiemaureen" target='_blank'>
+            <li className={styles.navItem}>Github</li>
+          </Link>
+          <Link className={styles.navLink} href="https://www.linkedin.com/in/codiemaureen/" target='_blank'>
+            <li className={styles.navItem}>LinkedIn</li>
+          </Link>
+          <Link className={styles.navLink} href="#projects">
+            <li className={styles.navItem}>Projects</li>
+          </Link>
+          <Link className={styles.navLink} href="#contactMe">
+            <li className={styles.navItem}>Contact Me</li>
+          </Link>
+        </ol> 
+      </div>
       <header className={styles.header}>
-        <img src='/pexels-pixabay-270632.jpg' alt='' className={styles.headerImage} />
+        <img src='/pexels-sarah-ann-3164915-7031427.jpg' alt='header-image' className={styles.headerImage} />
       </header>
-   
     </>
   )
 }
