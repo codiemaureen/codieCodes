1acddcdd3c53e577b02d5e649f95cc46317b6cf0
Codiemaureen <codiestover.cs@gmail.com>
2025-03-14 12:13:34 -0400

    Cleaning up contact me form

--- src/app/component/ContactForm.jsx
+++ src/app/component/ContactForm.jsx
@@ -30,42 +30,55 @@ const ContactForm = () => {
 
   return (
     <div className={styles.formContainer}>
-     <form action="" method="get" className={styles.form} onSubmit={onSubmit}>
-       <h2 className={styles.formHeading}>Let's Connect!</h2>
-       <div className={styles.emailInput}>
-         <label className={styles.label}  htmlFor="email">Enter Your Email: </label>
-         <input type="email" 
-         name="email" 
-         id="email" placeholder='Enter your subject here...' 
-         className={styles.formInput}
-         required />
-       </div>
-       <div className={styles.emailInput}>
-         <label 
-         className={styles.label} 
-         htmlFor="email">Enter Your Email Subject: </label>
-         <input 
-          type="text" 
-          name="email" 
-          id="email" 
-          className={styles.formInput}
-          autoComplete="off"
-          placeholder='example@gmail.com' required />
-       </div>
-       <div >
-       <div className={styles.textbody}>
-         <label className={styles.label} htmlFor="message">Enter Your Message:</label>
-         <textarea 
-         className={styles.textarea}
-         name="emailBody" 
-         rows={4} id="emailBody" 
-         placeholder='Enter your message here...'/>
-       </div>
-       </div>
-       <div >
-         <input type="submit" value="Send" className={`${styles.formInput} ${styles.button}`}/>
-       </div>
-     </form>
+      <form 
+      method="get" 
+      className={styles.form} 
+      onSubmit={onSubmit}>
+        <h2 className={styles.formHeading}>
+          Let's Connect!
+        </h2>
+        <div className={styles.emailInput}>
+          <label 
+            className={styles.label}          htmlFor="email">
+            Enter Your Email: 
+          </label>
+          <input 
+            type="email" 
+            name="email" 
+            id="email" 
+            placeholder='example@gmail.com'
+            className={styles.formInput}
+            required />
+        </div>
+        <div className={styles.emailInput}>
+          <label 
+          className={styles.label} 
+          htmlFor="email">Enter Your Email Subject: </label>
+          <input 
+            type="text" 
+            name="email" 
+            id="email" 
+            className={styles.formInput}
+            autoComplete="off"
+            placeholder='Enter your subject here...' required />
+        </div>
+        <div>
+          <div className={styles.textbody}>
+            <label className={styles.label} htmlFor="message">Enter Your Message:</label>
+            <textarea 
+            className={styles.textarea}
+            name="emailBody" 
+            rows={4} id="emailBody" 
+            placeholder='Enter your message here...'/>
+          </div>
+        </div>
+        <div>
+          <input 
+            type="submit" 
+            value="Send" 
+            className={`${styles.formInput} ${styles.button}`}/>
+        </div>
+      </form>
     </div>
   )
 }
