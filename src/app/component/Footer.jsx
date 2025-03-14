cafbfbc50ac2a53763caf7797bca03c2c9c9e5d3
Codiemaureen <codiestover.cs@gmail.com>
2025-03-14 13:29:18 -0400

    Styling footer

--- src/app/component/Footer.jsx
+++ src/app/component/Footer.jsx
@@ -6,37 +6,44 @@ const Footer = () => {
       <h3 className={styles.footerTitle}>Website Built By Codie Groth</h3>
       <section className={styles.credContainer}>
        <div className={styles.imageCredContainer}>
-        <h4 className={styles.imageCredit}>Image Credits</h4>
-        <ul className={styles.imageCred}>
-         <Link href={`https://www.pexels.com/photo/green-and-white-leaves-of-the-plant-7031427/`} target='_blank'>
+        <h4 className={styles.imageCredit}>Image Credits:</h4>
+        <ul className={styles.imageCredList}>
+         <Link className={styles.imageCred} href={`https://www.pexels.com/photo/green-and-white-leaves-of-the-plant-7031427/`} target='_blank'>
          <li className={styles.image}>
-            Photo by Sarah Ann: 
+            Header & Footer Photo by Sarah Ann
          </li>
          </Link>
         </ul>
        </div>
 
        <div className={styles.iconContainer}>
-        <ul className="iconCred">
-            <li className={styles.iconCred}>
-              <Link href='#navbar'>Home
+        <ul className={styles.icons}>
+            <li className={styles.icon}>
+              <Link href='#navbar'
+              className={styles.icon}>Home
               </Link>
             </li>
-          <li className={styles.iconCred}>
-            <Link href="https://github.com/codiemaureen" target='_blank'>
+          <li className={styles.icon}>
+            <Link href="https://github.com/codiemaureen" target='_blank'
+            className={styles.icon}>
               Github
             </Link>
           </li>
-          <li className={styles.iconCred}>
-            <Link href="https://www.linkedin.com/in/codiemaureen/" target='_blank'>LinkedIn
+          <li className={styles.icon}>
+            <Link 
+            href="https://www.linkedin.com/in/codiemaureen/" 
+            target='_blank'
+            className={styles.icon}>LinkedIn
             </Link>
           </li>
-          <li className={styles.iconC}>
-            <Link href="https://x.com/CodieMaureen" target='_blank'>Twitter
+          <li className={styles.icon}>
+            <Link href="https://x.com/CodieMaureen" target='_blank'
+            className={styles.icon}>Twitter
             </Link>
           </li>
-         <li className="icons">
-             <a href="mailto:codiegroth?subject=Hello%20there&body=I%20wanted%20to%20reach%20out">
+         <li className={styles.icon}>
+             <a href="mailto:codiegroth?subject=Hello%20there&body=I%20wanted%20to%20reach%20out"
+             className={styles.icon}>
                 CodieGroth@Gmail.com
               </a>
          </li>
