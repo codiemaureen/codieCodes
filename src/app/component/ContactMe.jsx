840af5027035294fedf3bb65a66c4bbfbc1adb8a
Codiemaureen <codiestover.cs@gmail.com>
2025-03-14 10:27:35 -0400

    Adding contact links

--- src/app/component/ContactMe.jsx
+++ src/app/component/ContactMe.jsx
@@ -1,31 +1,53 @@
+'use client';
 import styles from './ContactMe.module.css';
 import ContactForm from './ContactForm';
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import { PopupButton } from 'react-calendly';
 
 const ContactMe = () => {
+  const [rootElement, setRootElement] = useState(null);
+
+  useEffect(() => {
+    // Set the root element after the component mounts
+    setRootElement(document.getElementById("__next"));
+  }, []);
+
   return (
     <div id='contactMe' className={styles.contactContainer}>
-     <h2>{'<contact me />'}</h2>
-     <div className={styles.contactFlex}>
-     <ContactForm />
-
-     <ol className={styles.contactList}>
-      <li className={styles.contactItem}>
-       CodieGroth@gmail.com
-      </li>
-      <li className={styles.contactItem}>
-       586.864.3218
-      </li>
-      <li className={styles.contactItem}>
-       GitHub
-      </li>
-      <li className={styles.contactItem}>
-       LinkedIn
-      </li>
-      <li className={styles.contactItem}>
-       Schedule a Coffee Chat!
-      </li>
-     </ol>
-     </div>
+      <h2>{'contact me'}</h2>
+      <div className={styles.contactFlex}>
+        <ContactForm />
+        <ol className={styles.contactList}>
+            <li className={styles.contactItem}>    
+              <a href="mailto:codiegroth?subject=Hello%20there&body=I%20wanted%20to%20reach%20out">
+                CodieGroth@Gmail.com
+              </a>
+            </li>
+            <li className={styles.contactItem}>
+              <Link href="tel:+15868643218">
+                586.864.3218
+              </Link>
+            </li>
+            <li className={styles.contactItem}>
+              <Link href="https://github.com/codiemaureen" target='_blank'>
+                Github
+              </Link>
+            </li>
+            <li className={styles.contactItem}>
+              <Link href="https://www.linkedin.com/in/codiemaureen/" target='_blank'>LinkedIn
+              </Link>
+            </li>
+            <li className={styles.contactItem}>
+              <PopupButton
+                url="https://honzaap.github.io/GithubCity/?name=codiemaureen&year=2025" 
+                rootElement={rootElement}
+                text="Visit My Github City"
+                className={styles.modal}
+              />
+            </li>            
+        </ol>
+      </div>
     </div>
   )
 }
