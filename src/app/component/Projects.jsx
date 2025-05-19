f387ec78241e716c6b5de778bd2aee3374d91bdf
Codiemaureen <codiestover.cs@gmail.com>
2025-05-19 11:26:26 -0400

    Fix: only showing the latest projects

--- src/app/component/Projects.jsx
+++ src/app/component/Projects.jsx
@@ -4,7 +4,7 @@ import Project from "./Project";
 import styles from "./Projects.module.css"
 
 const ProjectList = () => {
-  const [projects, setProjects] = useState([]);
+  let [projects, setProjects] = useState([]);
 
   useEffect(() => {
     fetch("/projects.json")
@@ -12,11 +12,12 @@ const ProjectList = () => {
       .then((data) => setProjects(data.projects))
       .catch((error) => console.error("Error fetching projects:", error));
   }, []);
-
+  projects = projects.slice(0, 4);
   return (
     <div id="projects" className={styles.projects}>
       <h2 className={styles.projectsTitle} >{'My Projects'}</h2>
       <div className={styles.projectsContainer}>
+        
         {projects.map((project) => (
           <div key={project.id} className={styles.projectsItems}>
             <Project project={project} />
