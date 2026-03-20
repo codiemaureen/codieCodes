301dd8ead1a9b81b347668d358399bceb9cdef07
Codiemaureen <codiestover.cs@gmail.com>
2026-03-20 11:26:13 -0400

    Adding click to view on projets

--- src/app/component/Project.jsx
+++ src/app/component/Project.jsx
@@ -15,21 +15,47 @@ const Project = ({project}) => {
     setIsHovering(false);
   }
   return (
-   <div className={styles.projectCard} 
-   onMouseEnter={handleMouseEnter}
-   onMouseLeave={handleMouseLeave}>
-    <h3 className={styles.projectTitle}>{project.title}</h3>
-        <iframe 
+    <div
+      className={styles.projectCard}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      >
+      <h3 className={styles.projectTitle}>{project.title}</h3>
+
+      <div className={styles.iframeWrapper} >
+        <iframe
           src={project.website}
-          title={project.title} 
-          className={styles.projectImage} 
-          style={{ border: 'none' }} 
+          title={project.title}
+          className={styles.projectImage}
+          style={{ border: "none" }}
           loading="lazy"
         />
-    
+
+        <a
+          href={project.website}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.overlay}
+          aria-label={`Open ${project.title}`}
+        />
+      </div>
+
       <section className={styles.projectLinks}>
-        <Link className={styles.projectLink} href={project.githublink} target='_blank'>Github</Link>
-        <Link className={styles.projectLink} href={project.website} target='_blank'>Website</Link>
+        <Link
+          className={styles.projectLink}
+          href={project.githublink}
+          target="_blank"
+        >
+          Github
+        </Link>
+
+        <Link
+          className={styles.projectLink}
+          href={project.website}
+          target="_blank"
+        >
+          Website
+        </Link>
       </section>
     </div>
   )
