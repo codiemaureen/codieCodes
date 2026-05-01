d72def01a71cd6d3aad5ca5bd52cbd47875f6b7c
Codiemaureen <codiestover.cs@gmail.com>
2026-05-01 09:15:49 -0400

    Changing title

--- src/app/component/MySkills.jsx
+++ src/app/component/MySkills.jsx
@@ -4,7 +4,7 @@ const MySkills = () => {
   const skills = ['NEXT.JS', 'REACT', 'ANGULAR', 'NODE', 'MONGODB', 'AZURE', 'DOCKER', 'TYPESCRIPT', 'JAVASCRIPT', 'CSS/SASS', 'WEB ACCESSIBILITY']
   return (
     <div className={styles.skillsContainer}>
-      <h4 className={styles.listTitleMobile}>My Languages</h4>
+      <h4 className={styles.listTitleMobile}>Tech Stack</h4>
       <ol className={styles.skillList}>
       {skills.map((skill,i) => (
           <li key={i} className={styles.skillItems}>{skill}</li>
