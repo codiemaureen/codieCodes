cff08432111989caf50b7e86662fca31a3b31980
Codiemaureen <codiestover.cs@gmail.com>
2026-05-06 16:58:47 -0400

    updating footer

--- src/app/component/Footer.jsx
+++ src/app/component/Footer.jsx
@@ -1,61 +1,95 @@
 import styles from './Footer.module.css';
 import Link from 'next/link';
+
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
 
-  const currentYear= new Date().getFullYear();
   return (
-    <div className={styles.footer}>
-      <h3 className={styles.footerTitle}>Website Built By Codie Groth</h3>
-      <section className={styles.credContainer}>
-       <div className={styles.imageCredContainer}>
-        <h4 className={styles.imageCredit}>Image Credits:</h4>
-        <ul className={styles.imageCredList}>
-         <Link className={styles.imageCred} href={`https://www.pexels.com/photo/green-and-white-leaves-of-the-plant-7031427/`} target='_blank'>
-         <li className={styles.image}>
-            Header & Footer Photo by Sarah Ann
-         </li>
-         </Link>
-        </ul>
-       </div>
+    <footer className={styles.footer}>
+      <h3 className={styles.footerTitle}>
+        Website Built By Codie Groth
+      </h3>
 
-       <div className={styles.iconContainer}>
-        <ul className={styles.icons}>
-            <li className={styles.icon}>
-              <Link href='#navbar'
-              className={styles.icon}>Home
+      <section className={styles.credContainer}>
+        
+        {/* Image Credits */}
+        <div className={styles.imageCredContainer}>
+          <h4 className={styles.imageCredit}>Image Credits:</h4>
+          <ul className={styles.imageCredList}>
+            <li>
+              <a
+                href="https://www.pexels.com/photo/green-and-white-leaves-of-the-plant-7031427/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.imageCred}
+              >
+                Header & Footer Photo by Sarah Ann
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        {/* Navigation / Social */}
+        <div className={styles.iconContainer}>
+          <ul className={styles.icons}>
+            
+            <li>
+              <Link href="#navbar" className={styles.link}>
+                Home
               </Link>
             </li>
-          <li className={styles.icon}>
-            <Link href="https://github.com/codiemaureen" target='_blank'
-            className={styles.icon}>
-              Github
-            </Link>
-          </li>
-          <li className={styles.icon}>
-            <Link 
-            href="https://www.linkedin.com/in/codiemaureen/" 
-            target='_blank'
-            className={styles.icon}>LinkedIn
-            </Link>
-          </li>
-          <li className={styles.icon}>
-            <Link href="https://x.com/CodieMaureen" target='_blank'
-            className={styles.icon}>Twitter
-            </Link>
-          </li>
-         <li className={styles.icon}>
-             <a href="mailto:codiegroth?subject=Hello%20there&body=I%20wanted%20to%20reach%20out"
-             className={styles.icon}>
-                CodieGroth@Gmail.com
+
+            <li>
+              <a
+                href="https://github.com/codiemaureen"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.link}
+              >
+                GitHub
               </a>
-         </li>
+            </li>
 
-        </ul>
-        <p>&copy; { currentYear } Codie Groth. All rights reserved.</p>
-       </div>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/codiemaureen/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.link}
+              >
+                LinkedIn
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="https://x.com/CodieMaureen"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.link}
+              >
+                Twitter
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="mailto:codiegroth@gmail.com?subject=Hello&body=I%20wanted%20to%20reach%20out"
+                className={styles.link}
+              >
+                codiegroth@gmail.com
+              </a>
+            </li>
+
+          </ul>
+
+          <p className={styles.copy}>
+            &copy; {currentYear} Codie Groth. All rights reserved.
+          </p>
+        </div>
       </section>
-    </div>
-  )
-}
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
